08cc5cd8ce2bf0b236ab21a8da7a8bf839271a19
Watanabe Delhi Belly <watanabe.delhiberry@gmail.com>
2022-01-03 23:38:33 +0900

    yoを追加

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.<br/>
-          test Watanabe Desu
+          test Watanabe Desu yo
         </p>
         <a
           className="App-link"
